b4e8ca0eabea6199d3f8ad6e26c8651fc6dec136
Sonia Rao <soniahrao@gmail.com>
2018-11-03 01:33:58 -0400

    more stuff

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
-from scripts import *
+from scripts.data import *
 
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 def main():
 	if request.method == 'POST':
-		return render_template('processed.html')
+		data = do()
+		return render_template('processed.html',data=data)
    	return render_template('home.html')
 
 
